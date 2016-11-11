df289942ebccd610778772249f768f1782e4e4be
Brittany Brassell <brittany.brassell@gmail.com>
2016-11-10 22:13:07 -0800

    working version with a globe

--- twitter.js
+++ twitter.js
@@ -69,8 +69,7 @@ var Twitter = require('twitter'),
     credentials = require('./credentials.js'),
     client = new Twitter(credentials);
 
-//store query here, right now it's just a few things 'narrowing' down results
-var query = "trump" && 'profiling' && "violence" && "assault";
+var query = "trump";
 
 client.stream('statuses/filter', {track: query}, function(stream) {
   // Every time we receive a tweet...
