873ede25ff0ca5865fc6e5f0683075dfad6d63a0
Brittany Brassell <brittany.brassell@gmail.com>
2016-11-13 07:41:37 -0800

    prepping to plot points

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -15,12 +15,13 @@ var path = d3.geo.path()
     // .context(c); // for tweets 
 
 // Use canvas to draw tweets onto existing map??
+// not sure if these two are necessary, but there needs to be some connection to the socket
 var c = svg.node();
-console.log(c);
 var title = d3.select("#tweets");
 
 var defs = svg.append("defs");
 
+// queue the json needed to for map and then build in the socket connection
 queue()
     .defer(d3.json, "/world-110m.json")
     .defer(d3.tsv, "/world-country-names.tsv")
@@ -59,55 +60,56 @@ function ready(error, world, names, us) {
       .attr("class", "state-boundary")
       .attr("d", path);
 
-// 2. Open up a socket with Twitter, start stream AFTER map is rendered. 
+  // 2. Open up a socket with Twitter, start stream AFTER map is rendered. 
 
-var socket = io({ "force new connection" : true });
+  var socket = io({ "force new connection" : true });
 
-// TURN THIS INTO A TEMPLATE 
-function makeHTML(tweet) {
-  return [
-    '<div class="user">',
-    '<a href="https://twitter.com/', tweet.user, '" target="_blank">', '@', tweet.user, '</a>',
-    '<h1>', tweet.placeName, '</h1>',
-    '<div class="tweet">',
-    '<a href="https://twitter.com/', tweet.user, '/status/', tweet.id, '" target="_blank">',
-    tweet.text, '</a>', '</div>'
-  ].join('');
-}
-
-
-
-// 3. Show tweets on map
-socket.on('tweet', function(tweet) {
-  d3.transition()
-    .duration(1250)
-    .each("start", function() {
-      console.log(tweet);
-      $("#tweet").html(makeHTML(tweet));
-    });
-    // show each tweet with dot at lat long 
+  // TURN THIS INTO A TEMPLATE 
+  function makeHTML(tweet) {
+    return [
+      '<div class="user">',
+      '<a href="https://twitter.com/', tweet.user, '" target="_blank">', '@', tweet.user, '</a>',
+      '<h1>', tweet.placeName, '</h1>',
+      '<div class="tweet">',
+      '<a href="https://twitter.com/', tweet.user, '/status/', tweet.id, '" target="_blank">',
+      tweet.text, '</a>', '</div>'
+    ].join('');
+  }
 
 
-    // .tween("rotate", function() {
-    //   var p = tweet.latLong;
-    //       // r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
-    //   return function(t) {
-    //     // Rotate the earth so that the new point is front and center.
-    //     // projection.rotate(t); // rotate is maybe not right
-    //     // Erase the canvas.
-    //     c.clearRect(0, 0, width, height);
-    //     // Fill in all the landmasses gray.
-    //     c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill();
-    //     // Draw the country borders in white.
-    //     c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
-    //     // Draw the earth's circumference in black.
-    //     c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
-    //     // Get the canvas-coordinates of the latLong point, and draw a circle there.
-    //     // projection(p);
-    //     c.strokeStyle = "#000", c.fillStyle = "#f00", c.beginPath(), c.arc(center[0], center[1], 5, 0, 2 * Math.PI, false), c.lineWidth = 2, c.fill(), c.stroke();
-    //   };
-    // });
-});
+
+  // 3. Show tweets on map: this section needs to work nicely with the svg map
+  socket.on('tweet', function(tweet) {
+    d3.transition()
+      .duration(1250)
+      .each("start", function() {
+        console.log(tweet);
+
+        $("#tweet").html(makeHTML(tweet));
+      });
+      // show each tweet with dot at lat long 
+
+
+      // .tween("rotate", function() {
+      //   var p = tweet.latLong;
+      //       // r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+      //   return function(t) {
+      //     // Rotate the earth so that the new point is front and center.
+      //     // projection.rotate(t); // rotate is maybe not right
+      //     // Erase the canvas.
+      //     c.clearRect(0, 0, width, height);
+      //     // Fill in all the landmasses gray.
+      //     c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill();
+      //     // Draw the country borders in white.
+      //     c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
+      //     // Draw the earth's circumference in black.
+      //     c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
+      //     // Get the canvas-coordinates of the latLong point, and draw a circle there.
+      //     // projection(p);
+      //     c.strokeStyle = "#000", c.fillStyle = "#f00", c.beginPath(), c.arc(center[0], center[1], 5, 0, 2 * Math.PI, false), c.lineWidth = 2, c.fill(), c.stroke();
+      //   };
+      // });
+  });
 
 };
 d3.select(self.frameElement).style("height", height + "px");
